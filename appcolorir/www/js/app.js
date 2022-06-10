8c9723a47bec2eee7cc458e258e6f6f338214e71
Suellen <73621600+suellen05@users.noreply.github.com>
2022-06-09 22:21:00 -0300

    atualizacao

--- appcolorir/www/js/app.js
+++ appcolorir/www/js/app.js
@@ -1,13 +1,12 @@
 function MudarFundo(){
-    let corR = Math.floor(math.random() * 255)
-    let corG = Math.floor(math.random() * 255)
-    let corB = Math.floor(math.random() * 255)
-    let cor = 'rgb(${corR} , ${corG} , ${corB})' 
-console.log(cor)
-document.body.style.backgroundColor = cor
-}
-
-document.querySelector("#btColorir").addEventListener
-("click", function(){
-    MudarFundo()
-})
+    let corR = Math.floor(Math.random() * 255)
+    let corG = Math.floor(Math.random() * 255)
+    let corB = Math.floor(Math.random() * 255)
+    let cor =  `rgb(${corR},${corG},${corB})`
+   
+     document.body.style.backgroundColor = cor;
+ }
+ 
+ document.querySelector('#btColorir').addEventListener('click', function(){
+     MudarFundo();
+ })
